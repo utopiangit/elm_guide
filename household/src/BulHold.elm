d6c8d83b4a46ca947f45a7427fd51550d5659cc5
utopian <wuthering1118@gmail.com>
2019-05-26 16:03:06 +0900

    update view of history with Bulma

--- household/src/BulHold.elm
+++ household/src/BulHold.elm
@@ -10,10 +10,9 @@ import Bulma.Components exposing (..)
 import Bulma.Columns as Columns exposing (..)
 import Bulma.Layout exposing (..)
 
-import Html exposing ( Html, Attribute, main_, span, a, p, img ,br, text, strong, option, small, input, i, thead, tr, td, th, a)
-import Html.Attributes exposing ( attribute, style, src, placeholder, type_, href, rel, class )
+import Html exposing ( Html, Attribute, main_, span, a, p, img ,br, text, strong, option, small, input, i, thead, tr, td, th, a, div)
+import Html.Attributes exposing ( attribute, style, src, placeholder, type_, href, rel, class, value )
 import Html.Events exposing (..)
-
 
 type alias Shopping
   = { user : String
@@ -28,14 +27,19 @@ type alias Model
     , category : String
     , amount : String
     , memos : List Shopping
+    , tab : TabState
     }
 
 
 init : Model
-init = initialModel []
+init = initialModel [] History
 
-initialModel : List Shopping -> Model 
+initialModel : List Shopping -> TabState -> Model 
 initialModel = Model "Yuucha" "" "Food" "0"
+
+type TabState 
+  = Input
+  | History
 
 type Msg 
   = InputUser String
@@ -44,6 +48,9 @@ type Msg
   | InputAmount String
   | Submit
   | Delete Int
+  | InputTab
+  | HistoryTab
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -62,7 +69,8 @@ update msg model =
 
     Submit -> 
       initialModel 
-        <| (Shopping model.user model.date model.category model.amount) :: model.memos    
+        ((Shopping model.user model.date model.category model.amount) :: model.memos)
+        Input
 
     Delete n ->
       let
@@ -70,12 +78,18 @@ update msg model =
       in
         { model | memos = List.take n t ++ List.drop (n + 1) t }
 
+    InputTab -> 
+      { model | tab = Input }
+
+    HistoryTab -> 
+      { model | tab = History }
+
 main : Program () Model Msg
 main
   = Browser.sandbox
     { init = init
     , view = view
-    , update = \msg -> \model -> model
+    , update = update
     }
 
 fontAwesomeCDN
@@ -91,32 +105,110 @@ view model
     [ stylesheet
     , fontAwesomeCDN
     -- , exampleElementsAndComponents
-    , inputForm
+    , topHero
+    , tabsView model
+    , contentView model
     ]
 
-inputForm : Html Msg
-inputForm = section NotSpaced []
-    [ container []
-      [ demoSection "Shopping.." [ onSubmit Submit ]
+topHero : Html Msg
+topHero
+  = hero { heroModifiers | color = Info, size = Small } []
+      [ heroBody []
+        [ container []
+          [ title H2 [] [ text "HouseHold app" ] ]
+        ]
+      ]
+
+tabsView : Model -> Html Msg
+tabsView model = 
+  tabs { tabsModifiers | style = Minimal, alignment = Centered } [] []
+      [ tab (model.tab == Input) [ onClick InputTab ] [] 
+          [ icon Standard [] [ i [ class "fa fa-pencil" ] [] ], text "Input" ]
+      , tab (model.tab == History) [ onClick HistoryTab ] [] 
+          [ icon Standard [] [ i [ class "fa fa-history" ] [] ], text "History" ]
+      ]
+
+contentView model = 
+  case model.tab of
+          Input -> inputForm model
+          History -> historyView model
+
+historyView : Model -> Html Msg
+historyView model = section NotSpaced []
+    [ panel []
+        [ demoSection "Shoppings" []
+          ([ field []
+            [ controlLabel [] [ text "Recent items you bought.." ]
+            ]
+          ] ++ (viewShopping (List.indexedMap Tuple.pair model.memos)))
+        , demoSection "" [] 
+            [ table tableModifiers [] 
+                [ tableHead [] 
+                  [ tableRow False [] 
+                      [ tableCell [] [ text "Date" ]
+                      , tableCell [] [ text "User"]
+                      , tableCell [] [ text "Category"]
+                      , tableCell [] [ text "Amount"]
+                      ]
+                  ]
+                , tableBody [] 
+                  [ tableRow False [] 
+                      [ tableCell [] [ text "1, 1" ]
+                      , tableCell [] [ text "1, 2"]
+                      , tableCell [] [ text "1, 3"]
+                      ]
+                  , tableRow False [] 
+                      [ tableCell [] [ text "2, 1" ]
+                      , tableCell [] [ text "2, 2"]
+                      , tableCell [] [ text "2, 3"]
+                      ]
+                  ]
+                ] 
+            ]
+        ]
+    ]
+
+viewShopping : List (Int, Shopping) -> List (Html Msg)
+viewShopping hist = 
+  let
+    viewOne : (Int, Shopping) -> Html Msg
+    viewOne (n, shopping) = container [] 
+      [ text <| shopping.date ++ shopping.user ++ shopping.category ++ shopping.amount 
+      , controlButton { buttonModifiers | size = Small, color = Light } 
+          [ onClick (Delete n) ]
+          []
+          [ text "x" ]
+      ]
+  in List.map viewOne hist
+
+inputForm : Model -> Html Msg
+inputForm model = section NotSpaced []
+    [ container [ onSubmit Submit ]
+      [ demoSection "Register.." [ onSubmit Submit ]
         [ field []
           [ controlLabel [] [ text "Who you are" ]
-          , controlSelect controlSelectModifiers [ onInput InputUser ] []
+          , controlSelect controlSelectModifiers [ onInput InputUser ] [ value model.user ]
             [ option [] [ text "Yuucha" ]
             , option [] [ text "Naocha" ]
             ]
           ]
         , field []
           [ controlLabel [] [ text "When" ]
-          , controlInput controlInputModifiers [ type_ "date" ] [ placeholder "Date" ] [] 
+          , controlInput controlInputModifiers [ onInput InputDate ] 
+              [ placeholder "Date", type_ "date", value model.date ] [] 
           ]
-        , field []
-          [ controlLabel [] [ text "What you bought" ]
-          , controlInput controlInputModifiers [] [ placeholder "Item" ] [] 
-          , controlHelp Default [] [ text "This field isn't required!" ]
-          ]
+        -- , field []
+        --   [ controlLabel [] [ text "Where you bought" ]
+        --   , controlInput controlInputModifiers [] [ placeholder "Shop name" ] [] 
+        --   ]
+        -- , field []
+        --   [ controlLabel [] [ text "What you bought" ]
+        --   , controlInput controlInputModifiers [] [ placeholder "Item" ] [] 
+        --   , controlHelp Default [] [ text "This field isn't required!" ]
+        --   ]
         , field []
           [ controlLabel [] [ text "Category" ]
-          , controlSelect controlSelectModifiers [] []
+          , controlSelect controlSelectModifiers [ onInput InputCategory ] [ value model.category ]
             [ option [] [ text "Food" ]
             , option [] [ text "EatOut" ]
             , option [] [ text "Transportation" ]
@@ -126,16 +218,12 @@ inputForm = section NotSpaced []
             ]
           ]
         , field []
-          [ controlLabel [] [ text "Where you bought" ]
-          , controlInput controlInputModifiers [] [ placeholder "Shop name" ] [] 
-          ]
-        , field []
           [ controlLabel [] [ text "How much is it" ]
-          , controlInput controlInputModifiers [] [ placeholder "price" ] [] 
+          , controlInput controlInputModifiers [ onInput InputAmount ] [ placeholder "price", value model.amount ] [] 
           ]        
         , field []
-          [ controlButton { buttonModifiers | color = Link } [] []
-            [ text "Button"
+          [ controlButton { buttonModifiers | color = Link } [ onClick Submit ] []
+            [ text "Register"
             ]
           ]
         ]
