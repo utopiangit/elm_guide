4a2c60e69148b9361e612e2f2ec5a4c763c1c3a4
utopian <wuthering1118@gmail.com>
2019-08-25 17:29:22 +0900

    add stop button But it doesn't stop subscription

--- tutorial/src/elm_time.elm
+++ tutorial/src/elm_time.elm
@@ -2,6 +2,7 @@ import Browser
 import Html exposing (..)
 import Task
 import Time
+import Html.Events exposing (onInput, onClick)
 
 main = 
   Browser.element {
@@ -13,31 +14,40 @@ main =
 
 type alias Model = {
     zone : Time.Zone,
-    time : Time.Posix
+    time : Time.Posix,
+    isWorking : Bool
   }
 
 init : () -> (Model, Cmd Msg)
 init _ = 
-  (
-    Model Time.utc (Time.millisToPosix 0),
-    Task.perform AdjustTimeZone Time.here
+  ( Model Time.utc (Time.millisToPosix 0) True
+  , Task.perform AdjustTimeZone Time.here
   )
 
 type Msg
   = Tick Time.Posix
   | AdjustTimeZone Time.Zone
+  | Switch
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
   case msg of
     Tick newTime -> (
-      { model | time = newTime },
-      Cmd.none
+      if model.isWorking then
+        { model | time = newTime }
+      else 
+        model
+      , Cmd.none
      )
     AdjustTimeZone newZone -> (
-      { model | zone = newZone },
-      Cmd.none
+      { model | zone = newZone }
+      , Cmd.none
       )
+    Switch -> 
+      ( { model | isWorking = not model.isWorking }
+      , Cmd.none
+      )
+    
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -45,9 +55,16 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model = 
+  div [] 
+    [ timer model
+    , button [ onClick Switch ] [ text "Start/Stop" ] 
+    ]
+          
+timer : Model -> Html Msg
+timer model = 
   let
     hour   = String.fromInt (Time.toHour   model.zone model.time)
     minute = String.fromInt (Time.toMinute model.zone model.time)
     second = String.fromInt (Time.toSecond model.zone model.time)
   in
-  h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+    h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
